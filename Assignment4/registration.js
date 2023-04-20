200dca802be969251021c05f36cfdb58c710a58e
Jacob Dankel <55810804+JacobDankel@users.noreply.github.com>
2023-04-19 17:06:12 -0700

    Added Form and changed a bit of JS to validate inputs

--- Assignment4/registration.js
+++ Assignment4/registration.js
@@ -1,11 +1,19 @@
+var fname = "";
+var lname = "";
+var phone = "";
+var email = "";
+var dob = "";
+var country = "";
+var gender;
+
 function submitNewUser(){
-    var fname = document.getElementById("fName").value;
-    var lname = document.getElementById("lName").value;
-    var phone = document.getElementById("phoneNum").value;
-    var email = document.getElementById("email").value;
-    var dob = document.getElementById("DOB").value;
-    var country = document.getElementById("country").value;
-    var gender = document.getElementsByName('gender');
+    fname = document.getElementById("fName").value;
+    lname = document.getElementById("lName").value;
+    phone = document.getElementById("phoneNum").value;
+    email = document.getElementById("email").value;
+    dob = document.getElementById("DOB").value;
+    country = document.getElementById("country").value;
+    gender = document.getElementsByName('gender');
 
     //Showing Confirmation Box
 
@@ -61,5 +69,50 @@ function clearInputs() {
     for(i = 0; i < gender.length; i++) {
         if(gender[i].checked)
             gender[i].checked = false;
+    }
+}
+
+function validate(){
+    var status = false;
+    if(fname.length<1){
+        document.getElementById("fnameloc").innerText="Please enter your name";
+        status=false;
+    } else {
+        document.getElementById("fnameloc").innerText="";
+        status=true;
+    }
+    allLetters();
+    allDigits();
+}
+
+function allLetters(){
+    var letters = /^[A-Za-z]+$/;
+    if(document.getElementById("fNameResult").value.match(letters)){
+        return true;
+    } else {
+        document.getElementById("fnameloc").innerHTML="Your name should be all character values.";
+        return false;
+    }
+}
+
+function allDigits(){
+    var numbers = /^[0-9]+$/;
+
+    if(document.getElementById("phoneNumResult").value.match(numbers)){
+        return true;
+    } else {
+        document.getElementById("phoneloc").innerHTML="Your value should be all digits.";
+        return false;
+    }
+}
+
+function allDigits(){
+    var phoneno = /^\(?([0-9]{3})\)?[-.* ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+
+    if(document.getElementById("phoneNumResult").value.match(phoneno)){
+        return true;
+    } else {
+        document.getElementById("phoneloc").innerHTML="Your phone number should be all digits.";
+        return false;
     }
 }
